de98ec24f9fa2288a9766d02d0d22c5093a1f919
chaouachiolfa <chawachii.olfa@gmail.com>
2022-03-15 22:48:10 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import myImg from "./Assets/image.jpg"
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+    <div style={{ border:"solid 1px noir",maxWidth:"100vw"}}>
+
+<h1 className="title">
+  <span>B</span>
+  <span>o</span>
+  <span>n</span>
+  <span>j</span>
+  <span>o</span>
+  <span>u</span>
+  <span>r</span>
+  </h1>
+
+<br/>
+
+<img className='img' src={myImg} alt="myimg" />
+
+
+
+<img  className='imgg' src="/img.jpg" alt="imgg" />
+<br/>
+<iframe className='video' width="560" height="315" src="https://www.youtube.com/embed/tQzev_ZTrYQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+</div>
+
+
   );
 }
 
